c532d6570d449aa12212d652f6e78e7716ca9917
jshapoval <37222150+jshapoval@users.noreply.github.com>
2020-09-15 19:57:13 +0300

    FirstCommit

---
+++ Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string ContactInformation { get; set; }
+        public List<ContactPhoneNumber> PhoneNumbers { get; set; }
+        public string Email { get; set; }
+        public string Skype { get; set; }
+        public string Other { get; set; }
+
+    }
+
+}
---
+++ Models/ContactPhoneNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApp.Models
+{
+    public class ContactPhoneNumber
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
